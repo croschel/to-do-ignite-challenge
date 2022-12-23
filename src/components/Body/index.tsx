903e153149eff4a795818ea33da5526ad82ed890
croschel <croschel000@gmail.com>
2022-12-23 17:19:55 -0300

    check and uncheck control

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -1,27 +1,65 @@
-import React, { useState } from 'react';
-import clipboard from '../../assets/clipboard.svg';
-import plus from '../../assets/plus.svg';
-import styles from './styles.module.scss';
+import React, { useState } from "react";
+import { Task } from "../../@types/task";
+import clipboard from "../../assets/clipboard.svg";
+import plus from "../../assets/plus.svg";
+import { TaskItem } from "../TaskItem";
+import styles from "./styles.module.scss";
+
+const mock: Task[] = [
+  {
+    id: "1",
+    description: "Some description for test this...",
+    isDone: false,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    id: "2",
+    description:
+      "Some description for test this 2... addasdsadasdasdadadadas adasd asdaasd asd asd asdadadadsdasda",
+    isDone: true,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
 
 export const Body: React.FC = () => {
-  const [taskList, setTaskList] = useState([]);
+  const [taskList, setTaskList] = useState<Task[]>(mock);
   const [newTask, setNewTask] = useState("");
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-  }
+  };
+
+  const handleDeleteTask = (taskItem: Task) => {
+    const filteredTaskList = taskList.filter((task) => task.id !== taskItem.id);
+    setTaskList(filteredTaskList);
+  };
+
+  const handleCloseTask = (taskItem: Task) => {
+    const updatedTaskList = taskList.map((task) => {
+      if (taskItem.id === task.id) {
+        return {
+          ...task,
+          isDone: !taskItem.isDone,
+        };
+      }
+      return task;
+    });
+    setTaskList(updatedTaskList);
+  };
 
   return (
     <main className={styles.mainContainer}>
       <div className={styles.taskBarContainer}>
         <form onSubmit={handleSubmit}>
-          <input 
-            type="text" 
-            placeholder='Adicione uma nova tarefa' 
-            value={newTask} 
+          <input
+            type="text"
+            placeholder="Adicione uma nova tarefa"
+            value={newTask}
             onChange={(event) => setNewTask(event.target.value)}
           />
-          <button type='submit'>
+          <button type="submit">
             <p>Criar</p>
             <img src={plus} alt="plus" />
           </button>
@@ -40,24 +78,24 @@ export const Body: React.FC = () => {
             <div className={styles.counter}>0</div>
           </div>
         </div>
-        {taskList.length === 0 
-          ? (
-            <>
-              <div className={styles.emptyTaskBox}>
-                <img src={clipboard} alt="clipboard" />
-                <p>Você ainda não tem tarefas cadastradas</p>
-                <p>Crie tarefas e organize seus itens a fazer</p>
-              </div>
-            </>
-          )
-          : (
-            <div>
-              List
-            </div>
-          )
-        }
-
+        {taskList.length === 0 ? (
+          <div className={styles.emptyTaskBox}>
+            <img src={clipboard} alt="clipboard" />
+            <p>Você ainda não tem tarefas cadastradas</p>
+            <p>Crie tarefas e organize seus itens a fazer</p>
+          </div>
+        ) : (
+          <div className={styles.taskListBox}>
+            {taskList.map((task) => (
+              <TaskItem
+                task={task}
+                onDelete={handleDeleteTask}
+                onClose={handleCloseTask}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </main>
   );
-}
+};
---
+++ src/components/TaskItem/index.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { Task } from "../../@types/task";
+import trash from "../../assets/trash.svg";
+import check from "../../assets/check.svg";
+import styles from "./styles.module.scss";
+
+interface Props {
+  task: Task;
+  onDelete: (task: Task) => void;
+  onClose: (task: Task) => void;
+}
+
+export const TaskItem = ({ task, onDelete, onClose }: Props) => {
+  return (
+    <div className={styles.taskItemContainer}>
+      <div className={styles.checkboxTask}>
+        {task.isDone ? (
+          <img
+            src={check}
+            alt={`task ${task.isDone} checkbox`}
+            onClick={() => onClose(task)}
+          />
+        ) : (
+          <div className={styles.checkBoxInput} onClick={() => onClose(task)} />
+        )}
+        <span>{task.description}</span>
+      </div>
+      <button onClick={() => onDelete(task)} className={styles.removeButton}>
+        <img src={trash} alt="remove task icon" />
+      </button>
+    </div>
+  );
+};
